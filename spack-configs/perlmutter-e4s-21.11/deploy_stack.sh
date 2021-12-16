9a9ee78c7304083e9394c91d7489ecd543b7873d
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2021-12-15 17:07:31 -0800

    relocate file structure and add all the previous e4s configuration files

--- spack-configs/perlmutter-e4s-21.11/deploy_stack.sh
+++ spack-configs/perlmutter-e4s-21.11/deploy_stack.sh
@@ -4,7 +4,7 @@ source $spack_root/share/spack/setup-env.sh
 export SPACK_DISABLE_LOCAL_CONFIG=true
 spack env deactivate
 spack env rm -y e4s
-spack env create e4s  prod/spack.yaml
+spack env create e4s  spack.yaml
 spack env activate e4s
 spack install 
 spack module lmod refresh --delete-tree -y
--- spack-configs/perlmutter-e4s-21.11/get_spack.sh
+++ spack-configs/perlmutter-e4s-21.11/get_spack.sh
@@ -2,4 +2,3 @@
 spack_root=/global/common/software/spackecp/perlmutter/e4s-21.11/spack
 rm -rf $spack_root
 git clone git@github.com:spack/spack.git -b e4s-21.11 $spack_root
-cd $spack_root
