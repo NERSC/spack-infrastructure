01262b019adb4b684860716e229a8f0f6de7daab
Shahzeb Siddiqui <shahzebsiddiqui@lbl.gov>
2022-01-05 16:13:58 +0000

    add netlibscalapack and mvapich2 to systemlayer

--- spack-configs/perlmutter-systemlayer/ci/setup.sh
+++ spack-configs/perlmutter-systemlayer/ci/setup.sh
@@ -14,9 +14,9 @@ spack-python --path
 cd $CI_PROJECT_DIR
 
 # need to set line below to cherry-pick commit
-#git config user.name 'e4s' 
-#git config user.email 'e4s@nersc.gov'
+git config user.name 'e4s' 
+git config user.email 'e4s@nersc.gov'
 # NVHPC 21.11 - https://github.com/spack/spack/pull/27910
-#git cherry-pick 5d6a9a7
+git cherry-pick 5d6a9a7
 
 spack env activate -d $CI_PROJECT_DIR/spack-configs/perlmutter-systemlayer/ci
