f2e7b41172ea24317ad45ca4d37f2be3e6bee3c4
Shahzeb Siddiqui <shahzebsiddiqui@lbl.gov>
2022-01-05 15:39:31 +0000

    update to git tag 0.17.1 when building system layer

--- spack-configs/perlmutter-systemlayer/ci/setup.sh
+++ spack-configs/perlmutter-systemlayer/ci/setup.sh
@@ -6,13 +6,17 @@ which python && pip list
 rm -rf ~/.spack/
 export SPACK_DISABLE_LOCAL_CONFIG=true
 cd $CI_PROJECT_DIR
-git clone -b e4s-21.11 https://github.com/spack/spack
-. spack/share/spack/setup-env.sh
-spack-python --path
+git clone https://github.com/spack/spack
 cd spack
+git checkout tags/0.17.1
+. share/spack/setup-env.sh
+spack-python --path
+cd $CI_PROJECT_DIR
+
 # need to set line below to cherry-pick commit
-git config user.name 'e4s' 
-git config user.email 'e4s@nersc.gov'
+#git config user.name 'e4s' 
+#git config user.email 'e4s@nersc.gov'
 # NVHPC 21.11 - https://github.com/spack/spack/pull/27910
-git cherry-pick 5d6a9a7
+#git cherry-pick 5d6a9a7
+
 spack env activate -d $CI_PROJECT_DIR/spack-configs/perlmutter-systemlayer/ci
