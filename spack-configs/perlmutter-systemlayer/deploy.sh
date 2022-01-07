fdd58555ec6faa69d8802dbb153e216a7e72c30d
Shahzeb Siddiqui <shahzebsiddiqui@lbl.gov>
2022-01-07 15:09:24 +0000

    fix deployment script for systemlayer

--- spack-configs/perlmutter-systemlayer/deploy.sh
+++ spack-configs/perlmutter-systemlayer/deploy.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 python3 -m venv $CI_PROJECT_DIR/spack-pyenv
 source $CI_PROJECT_DIR/spack-pyenv/bin/activate
-pip install clingo  
-which python && pip list 
+pip install clingo
+which python && pip list
 rm -rf ~/.spack/
 export SPACK_DISABLE_LOCAL_CONFIG=true
 cd $CI_PROJECT_DIR
 systemlayer=/global/common/software/spackecp/perlmutter/systemlayer/
-spack_root=/global/common/software/spackecp/perlmutter/systemlayer/spack
+spack_root=$systemlayer/spack
+rm -rf $systemlayer
 mkdir -p $systemlayer
-rm -rf $spack_root
 cd $systemlayer
-git clone git@github.com:spack/spack.git -b  v0.17.1 $spack_root
+git clone https://github.com/spack/spack.git -b  v0.17.1 $spack_root
 export SPACK_PYTHON=$(which python)
 . $spack_root/share/spack/setup-env.sh
 spack --version
 spack-python --path
 spack env create systemlayer $CI_PROJECT_DIR/spack-configs/perlmutter-systemlayer/ci/spack.yaml
+spack env activate systemlayer
+spack env st
 spack install
 spack module tcl refresh --delete-tree -y
 spack find
