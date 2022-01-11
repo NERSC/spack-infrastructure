44d86966d22bbc987f37d7a628f0baf2c7e74d40
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2022-01-11 11:16:26 -0800

    adding muller CI job for e4s/21.11 with a spack.yaml add setup-env.sh script that provisions a python environment to ensure we have clean environment when building stack

---
+++ setup-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$CI_PROJECT_DIR" ]; then
+	CI_PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+fi
+python3 -m venv $CI_PROJECT_DIR/spack-pyenv
+source $CI_PROJECT_DIR/spack-pyenv/bin/activate
+pip install clingo  
+which python && pip list 
+rm -rf ~/.spack/
+export SPACK_DISABLE_LOCAL_CONFIG=true
+export SPACK_PYTHON=$(which python)
