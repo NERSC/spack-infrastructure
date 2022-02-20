af94dbe366a053d08aa00e1f4e3b615833fae752
Shahzeb Siddiqui <shahzebsiddiqui@lbl.gov>
2022-02-20 04:52:15 +0000

    create spack.yaml for cori e4s-22.02 production instance. Add a update_buildcache.sh script that can automate binary signing for arbitrary mirror path. One needs to specify full location of mirror. add production gitlab job for cori e4s 22.02

---
+++ update_buildcache.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export SPACK_GNUPGHOME=$HOME/.gnupg
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied for mirror path. Please specify full path to spack mirror "
+    exit 1
+fi
+
+E4S_MIRROR=$1
+rm -rf $E4S_MIRROR
+for ii in $(spack find --format "yyy {version} /{hash}" |
+	    grep -v -E "^(develop^master)" |
+	    grep "yyy" |
+	    cut -f3 -d" ")
+do
+  spack buildcache create -af -d $E4S_MIRROR --only=package $ii
+done
+
+spack buildcache update-index -d $E4S_MIRROR
+
