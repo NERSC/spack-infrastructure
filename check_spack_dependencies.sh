8b67006293f218be355ed656b6c3a6222c2406eb
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2023-11-02 08:38:39 -0700

    fix compiler versions for cce

--- check_spack_dependencies.sh
+++ check_spack_dependencies.sh
@@ -15,7 +15,7 @@ fi
 if [ "$NERSC_HOST" == "perlmutter" ]; then
 	nvhpc_versions=( 21.11 22.7 23.1)
 	nvhpc_modules=(nvhpc/21.11 nvhpc/22.7 nvhpc/23.1 )
-	cce_modules=( cce/15.0.0 )
+	cce_modules=( cce/15.0.1 cce/16.0.0 )
 	gcc_modules=( gcc/10.3.0 gcc/11.2.0 gcc/12.2.0 )
 	cray_mpich_version=8.1.25
 	cray_libsci_version=23.02.1.1
@@ -26,7 +26,7 @@ if [ "$NERSC_HOST" == "perlmutter" ]; then
 elif [ "$NERSC_HOST" == "muller" ]; then
 	nvhpc_versions=( 21.11 22.7 23.1)
 	nvhpc_modules=( nvhpc/21.11 nvhpc/22.7 nvhpc/23.1)
-	cce_modules=( cce/15.0.0 )
+	cce_modules=( cce/15.0.1 cce/16.0.0 )
 	gcc_modules=( gcc/10.3.0 gcc/11.2.0 gcc/12.2.0 )
 	cray_mpich_version=8.1.25
 	cray_libsci_version=23.02.1.1
